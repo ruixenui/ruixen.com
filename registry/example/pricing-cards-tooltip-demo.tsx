4770bc243c2ccb1fdc73da098ab883b976acbabf
SriSomanaath <govindalapudisrinath@gmail.com>
2026-03-26 23:09:40 +0530

    add pricing cards tooltip component

---
+++ registry/example/pricing-cards-tooltip-demo.tsx
@@ -0,0 +1,9 @@
+import PricingCardsTooltip from "@/registry/ruixenui/pricing-cards-tooltip";
+
+export default function PricingCardsTooltipDemo() {
+  return (
+    <div className="w-full">
+      <PricingCardsTooltip />
+    </div>
+  );
+}
